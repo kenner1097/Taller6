1b4bd2d9503eee818b37df3e208f39bcc4a324fa
Kenner QM <Kenner QM@DESKTOP-9KH7B3E>
2016-09-08 21:46:04 -0500

    new comit

---
+++ src/cercano/Cercano.java
@@ -0,0 +1,34 @@
+package cercano;
+
+/**
+ *
+ * @author Kenner QM
+ */
+public class Cercano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     int medio=2;
+     int mayor=3;
+     int menor=1;
+     int cercano=0;
+     if (((medio-mayor)==(-1))&&((medio-menor)==(1)))
+                    { 
+                     System.out.print("el mayor y el menor estan a la maisma distancia que el del medio  ");
+                    }  else {      
+     if ((medio/mayor)<=1.0)
+     {
+      cercano=mayor;
+     } else
+         {
+          if ((medio/menor)<=1.0) 
+           { 
+            cercano=menor;
+           } 
+         }
+    System.out.println(cercano);
+    }
+  }
+}
